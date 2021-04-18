039c63e4f2f8ff5519aa94160d81c7893a9767e3
erwin <unsuscriptormas99@gmail.com>
2021-04-17 20:20:58 -0600

    getElementById update

--- js/loginModule.js
+++ js/loginModule.js
@@ -11,8 +11,8 @@ try{
 // Función para inicar sesión
 function login(){
     // Obteniendo valores de los inputs usuario y contraseña
-    let userName = document.querySelector("#usernameInput").value;
-    let password = document.querySelector("#passwordInput").value;
+    let userName = document.getElementById("#usernameInputL").value;
+    let password = document.getElementById("#passwordInputL").value;
     
     // Este if verificará que los input de usuario y contraseña no esten vacios
     if(userName != "" && password != ""){
@@ -50,3 +50,56 @@ function login(){
         window.alert("Debes llenar Todos los campos para poder iniciar Sesión")
     }
 }
+
+
+
+try{
+    let newPatientBtn = document.querySelector("#redRegister");
+    newPatientBtn.addEventListener("click",create_patient);
+    
+} catch (error) {
+    
+}
+
+
+// Función para crear usuario
+function create_patient(){
+    // Obteniendo valores de los inputs
+    let name = document.getElementById("#nameInputR").value;
+    let lastName = document.getElementById("#lastnameInputR").value;
+    let userName = document.getElementById("#usernameInputR").value;
+    let password = document.getElementById("#passwordInputR").value;
+    let birth = document.getElementById("#birthInputR")
+    let gender = document.getElementById("#genderListR").value;
+    let phone = document.getElementById("#phoneInputR")
+        // Haciendo una petición al servidor
+        fetch("https://ipc1project2.herokuapp.com/registerr",{
+            method:"POST",
+            headers:{
+                'Content-Type':'application/json'
+            },
+            body: JSON.stringify({
+                "name": name,
+                "last_name": lastName,
+                "user_name": userName,
+                "password":password,
+                "gender":gender,
+                "date_of_birth":birth,
+                "phone": phone
+                
+            })
+        }).then(res => res.json())
+        .catch(err  => {
+            window.alert("Ocurrio un error al intentar crear tu usuario")
+        })
+        .then(res => {
+            // Verificando estado de respuesta y cambiando vista a login
+            if(res.state == "ok"){
+                window.alert("Tu usuario ha sido creado con éxito")
+                window.location = "adminView.html"
+            }
+            else{
+                window.alert("El nombre de usuario no está disponible")
+            }
+        })
+    }
