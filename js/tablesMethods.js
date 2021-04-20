bf752c2cbcad207d2464977fee298ecd727bb558
erwin <unsuscriptormas99@gmail.com>
2021-04-19 22:00:16 -0600

    new petitions

--- js/tablesMethods.js
+++ js/tablesMethods.js
@@ -4,182 +4,141 @@ logoutBtn.addEventListener("click",function(){
     localStorage.removeItem("logged");
     localStorage.removeItem("user_name");
     localStorage.removeItem("password");
+    //localStorage.removeItem("role");
 });
 
-function get_patients() {
 
-    get_data();
 
-    var url = '';
 
-    fetch(url, {
-        method: 'GET',
-        headers: {
-            'Content-Type': 'application/json'
+
+
+get_patients()
+get_doctors()
+get_nurses()
+get_medicines()
+get_id_of_users()
+function get_patients(){
+    let patientsTable = document.getElementById("tablapacientes")
+    fetch("http://127.0.0.1:5000/get-patients",{
+        method:"GET",
+        headers:{
+            'Content-Type':'application/json'
         }
+    }).then(res => res.json())
+    .catch(err  => {
+        console.log("ha ocurrido un error"+err)
     })
-        .then(res => res.json())
-        .then(function (response) {
-            var array = response.data;
-            console.log(array)
-
-            var new_html = ""
-
-            for (let i = 0; i < array.length; i++) {
-                new_html += `\n<tr>
-                <td>`+ array[i].id + `</td>
-                <td>`+ array[i].name + `</td>
-                <td>`+ array[i].last_name + `</td>
-                <td>`+ array[i].user_name + `</td>
-                <td>`+ array[i].user_role + `</td>
+    .then(res => {
+        res.forEach(user => {
+            patientsTable.innerHTML = patientsTable.innerHTML + `
+            <tr>
+                <td>${user.id} </td>
+                <td>${user.name} </td>
+                <td>${user.last_name} </td>
+                <td>${user.user_name} </td>
+                <td>${user.user_role} </td>
             </tr>`
-
-            }
-
-            $('#body_table').append(new_html);
-
-            get_ratings(array);
-
-        })
-        .catch(function (error) {
-            console.log(error)
-        })
-
-
-
+            
+        });
+    })        
 }
 
 
-
-
-function get_doctors() {
-
-    get_data();
-
-    var url = '';
-
-    fetch(url, {
-        method: 'GET',
-        headers: {
-            'Content-Type': 'application/json'
+function get_doctors(){
+    let doctorsTable = document.getElementById("tabladoc")
+    fetch("http://127.0.0.1:5000/get-doctors",{
+        method:"GET",
+        headers:{
+            'Content-Type':'application/json'
         }
+    }).then(res => res.json())
+    .catch(err  => {
+        console.log("ha ocurrido un error"+err)
     })
-        .then(res => res.json())
-        .then(function (response) {
-            var array = response.data;
-            console.log(array)
-
-            var new_html = ""
-
-            for (let i = 0; i < array.length; i++) {
-                new_html += `\n<tr>
-                <td>`+ array[i].id + `</td>
-                <td>`+ array[i].name + `</td>
-                <td>`+ array[i].last_name + `</td>
-                <td>`+ array[i].user_role + `</td>
-                <td>`+ array[i].speciality + `</td>
+    .then(res => {
+        res.forEach(user => {
+            doctorsTable.innerHTML = doctorsTable.innerHTML + `
+            <tr>
+                <td>${user.id} </td>
+                <td>${user.name} </td>
+                <td>${user.last_name} </td>
+                <td>${user.user_role} </td>
+                <td>${user.speciality} </td>
             </tr>`
-
-            }
-
-            $('#body_table').append(new_html);
-
-            get_ratings(array);
-
-        })
-        .catch(function (error) {
-            console.log(error)
-        })
-
-
-
+            
+        });
+    })        
 }
 
-
-
-function get_nurses() {
-
-    get_data();
-
-    var url = '';
-
-    fetch(url, {
-        method: 'GET',
-        headers: {
-            'Content-Type': 'application/json'
+function get_nurses(){
+    let nursesTable = document.getElementById("tablanurse")
+    fetch("http://127.0.0.1:5000/get-nurses",{
+        method:"GET",
+        headers:{
+            'Content-Type':'application/json'
         }
+    }).then(res => res.json())
+    .catch(err  => {
+        console.log("ha ocurrido un error"+err)
     })
-        .then(res => res.json())
-        .then(function (response) {
-            var array = response.data;
-            console.log(array)
-
-            var new_html = ""
-
-            for (let i = 0; i < array.length; i++) {
-                new_html += `\n<tr>
-                <td>`+ array[i].id + `</td>
-                <td>`+ array[i].name + `</td>
-                <td>`+ array[i].last_name + `</td>
-                <td>`+ array[i].user_name + `</td>
-                <td>`+ array[i].user_role + `</td>
+    .then(res => {
+        res.forEach(user => {
+            nursesTable.innerHTML = nursesTable.innerHTML + `
+            <tr>
+                <td>${user.id} </td>
+                <td>${user.name} </td>
+                <td>${user.last_name} </td>
+                <td>${user.user_name} </td>
+                <td>${user.user_role} </td>
             </tr>`
-
-            }
-
-            $('#body_table').append(new_html);
-
-            get_ratings(array);
-
-        })
-        .catch(function (error) {
-            console.log(error)
-        })
-
-
-
+            
+        });
+    })        
 }
 
-
-
-function get_medicines() {
-
-    get_data();
-
-    var url = '';
-
-    fetch(url, {
-        method: 'GET',
-        headers: {
-            'Content-Type': 'application/json'
+function get_medicines(){
+    let medTable = document.getElementById("tablamed")
+    fetch("http://127.0.0.1:5000/get-medicines",{
+        method:"GET",
+        headers:{
+            'Content-Type':'application/json'
         }
+    }).then(res => res.json())
+    .catch(err  => {
+        console.log("ha ocurrido un error"+err)
     })
-        .then(res => res.json())
-        .then(function (response) {
-            var array = response.data;
-            console.log(array)
-
-            var new_html = ""
-
-            for (let i = 0; i < array.length; i++) {
-                new_html += `\n<tr>
-                <td>`+ array[i].id + `</td>
-                <td>`+ array[i].name + `</td>
-                <td>`+ array[i].price + `</td>
-                <td>`+ array[i].amount + `</td>
+    .then(res => {
+        res.forEach(medicine => {
+            medTable.innerHTML = medTable.innerHTML + `
+            <tr>
+                <td>${medicine.id} </td>
+                <td>${medicine.name} </td>
+                <td>${medicine.price} </td>
+                <td>${medicine.amount} </td>
             </tr>`
-
-            }
-
-            $('#body_table').append(new_html);
-
-            get_ratings(array);
-
-        })
-        .catch(function (error) {
-            console.log(error)
-        })
-
-
-
+            
+        });
+    })        
 }
+
+function get_id_of_users(){
+    let selectionOfId = document.getElementById("idList")
+    fetch("http://127.0.0.1:5000/get-id_users",{
+        method:"GET",
+        headers:{
+            'Content-Type':'application/json'
+        }
+    }).then(res => res.json())
+    .catch(err  => {
+        console.log("ha ocurrido un error"+err)
+    })
+    .then(res => {
+        res.forEach(user => {
+            selectionOfId.innerHTML = selectionOfId.innerHTML + `
+            
+                <option>${user.id} </option>
+`        
+        });
+    })        
+}
+
